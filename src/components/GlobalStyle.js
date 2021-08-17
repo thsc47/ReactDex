c3202ec87aa5a69184deaa01c7cd51267442bf65
Thiago Henrique <thsc47@gmail.com>
2021-08-17 11:19:53 -0300

    Navbar CSS converted to styled-components

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-.nav-link{
-    font-family: 'Pokemon Solid';
-}  
  
 *{
        box-sizing: border-box;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,31 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-import Navbackground from '../../assets/images/navbackground.gif'
-import  styled  from "styled-components";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Navbackground from "../../assets/images/navbackground.gif";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Pokemon Solid";
+`;
+
+const NavLink = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-left: 300px;
+`;
+
+const NavBackground = styled.img`
+  height: 180px;
+  width: 92%;
+`;
 
 const Titulo = styled.h1`
-    font-family: 'Pokemon Solid';
+  font-family: "Pokemon Solid";
+  margin-top: -150px;
+  font-size: 70px;
 `;
 
 const About = styled.h2`
-font-family: 'Pokemon Solid';
+  font-family: "Pokemon Solid";
+  margin-top: -200px;
+`;
+
+const A = styled.a`
+  text-decoration: none;
+  color: blue;
+  hover {
+  text-decoration: none;
+  font-size: 120%;
+  color: yellow;
 `;
 
 const Navbar = () => {
-    return (
-        <>
-        <nav className='nav-bar'>
-            <img className='nav-background' src={Navbackground} alt='logo'/>                   
-        </nav>
-       
-        <div className='nav-link'>
-            <Link to='/home'><Titulo>ReactDex</Titulo></Link>            
-            <Link to='/about'><About>About us</About></Link> 
-        </div>
-        </>
-    )        
-    
-}
+  return (
+    <>
+      <Nav>
+        <NavBackground src={Navbackground} alt="logo" />
+      </Nav>
 
-export default Navbar
+      <NavLink>
+        <Link to="/home">
+          <Titulo>ReactDex</Titulo>
+        </Link>
+        <Link to="/about">
+          <About>About us</About>
+        </Link>
+      </NavLink>
+    </>
+  );
+};
+
+export default Navbar;
