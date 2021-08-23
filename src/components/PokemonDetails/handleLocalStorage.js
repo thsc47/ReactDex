82651412b226e92bf61f071c7a11d79a542ecf46
Thiago Henrique <thsc47@gmail.com>
2021-08-22 23:01:12 -0300

    Working delete on local storage

--- src/components/PokemonDetails/handleLocalStorage.js
+++ src/components/PokemonDetails/handleLocalStorage.js
@@ -1,7 +1,12 @@
 const handleLocalStorage = (name) => {
-    let party = JSON.parse(localStorage.getItem('party-member')) || [];
+  let party = JSON.parse(localStorage.getItem("party-member")) || [];
+  const index = party.indexOf(name);
+  if (index !== -1) {
+    party.splice(index, 1);
+  } else {
     party.push(name);
-    localStorage.setItem('party-member', JSON.stringify(party))   
-}
+  }
+  localStorage.setItem("party-member", JSON.stringify(party));
+};
 
-export default handleLocalStorage
+export default handleLocalStorage;
